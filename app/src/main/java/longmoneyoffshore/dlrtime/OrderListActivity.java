dc2d431a2732909fa5e7f6f348251a90e029480e
danolaru <danolaru@gmail.com>
2018-09-27 23:57:41 +0200

    my working version of the app with no working login and with minimum google sheets functionality

--- app/src/main/java/longmoneyoffshore/dlrtime/OrderListActivity.java
+++ app/src/main/java/longmoneyoffshore/dlrtime/OrderListActivity.java
@@ -172,8 +172,6 @@ public class OrderListActivity extends AppCompatActivity {
 
     private ClientAdapter passBackOrderChanges () {
 
-        //return modifications to GSheets
-
         //clients.set(positionOnListClicked, returnLocalClient);
         //final ClientAdapter reAdapter = new ClientAdapter(this, R.layout.client_item, clients);
         //listview.setAdapter(reAdapter);
