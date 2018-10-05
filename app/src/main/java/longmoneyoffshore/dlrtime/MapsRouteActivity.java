28108416e14294462e33c8d614b477024aa55fbf
danolaru <danolaru@gmail.com>
2018-10-05 04:16:11 +0200

    Implemented generate route between points, next TODO: optimize route

--- app/src/main/java/longmoneyoffshore/dlrtime/MapsRouteActivity.java
+++ app/src/main/java/longmoneyoffshore/dlrtime/MapsRouteActivity.java
@@ -1,22 +1,30 @@
 package longmoneyoffshore.dlrtime;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 //extra
 import android.graphics.Color;
 import android.os.AsyncTask;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,11 +37,13 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.api.client.http.HttpResponse;
 
 import org.json.JSONArray;
@@ -47,6 +57,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -55,14 +66,26 @@ import longmoneyoffshore.dlrtime.utils.DirectionsJSONParser;
 import longmoneyoffshore.dlrtime.utils.GetCoordinates;
 import longmoneyoffshore.dlrtime.utils.MapDestinationsParcel;
 import static longmoneyoffshore.dlrtime.utils.GlobalValues.APP_API_KEY;
+import static longmoneyoffshore.dlrtime.utils.GlobalValues.ChicagoLocale;
 
-public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnPolylineClickListener {
 
     private GoogleMap mMap;
-    ArrayList markerPoints= new ArrayList();
+    ArrayList<LatLng> markerPoints= new ArrayList <LatLng> ();
     MapDestinationsParcel locationsToSee;
     ArrayList<LatLng> locationsToSeeCoordinates = new ArrayList<LatLng>();
     final String userLocale = "chicago"; // this will be the general locale of the user
+    final LatLng userLocalePos = ChicagoLocale;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LatLng currentUserGeoPosition;
+
+    //Dan: experimetnal---------------------
+
+    private static final float PATTERN_GAP_LENGTH_PX = 2;
+    private static final PatternItem DOT = new Dot();
+    private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
+    private static final List<PatternItem> PATTERN_POLYLINE_DOTTED = Arrays.asList(GAP, DOT);
 
     public int counter = 0;
 
@@ -71,6 +94,31 @@ public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_route);
 
+        ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        if (ActivityCompat.checkSelfPermission(MapsRouteActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(MapsRouteActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsRouteActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }else{
+            // Write you code here if permission already given.
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            mFusedLocationClient.getLastLocation().
+                    addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null)  {
+                                LatLng lastLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                currentUserGeoPosition = lastLatLng;
+                            }
+                        }
+                    });
+        }
+
+        //dummy location for testing
+        currentUserGeoPosition = ChicagoLocale;
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -78,14 +126,8 @@ public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCal
         Bundle getLocationsToSeeBundle = getIntent().getExtras();
         locationsToSee = (MapDestinationsParcel) getLocationsToSeeBundle.getParcelable("locations to go to");
         locationsToSeeCoordinates.clear();
-        //TODO: transform locations to see, which is a list of String addresses, to a list of LatLng coordinates
 
-        //dan
-        //TODO: get coordinates for a ArrayList of addresses
-        //Log.d("NUMBEROFLOCSTOSEE", "there are " + locationsToSee.getMapDestinationLocations().size() + " locations");
-
-        //for (int j=0; j<locationsToSee.getMapDestinationLocations().size(); j++)
-
+        //TODO: make it an ASyncTask because it perceptibly slows down the transition from OrdersListActivity to MapsRouteActivity
         LatLng geoCoords;
 
         for (int j=0; j<locationsToSee.getMapDestinationLocations().size(); j++) {
@@ -106,35 +148,6 @@ public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCal
         }
     }
 
-    private LatLng geoLocateString (String addressInput) throws IOException{
-
-        Geocoder gc = new Geocoder(MapsRouteActivity.this);
-        List<Address> listAddress = gc.getFromLocationName(addressInput, 1);
-
-        Address coords;
-        if (listAddress.size()>0) {coords = listAddress.get(0);}
-        else {
-            coords = null;
-            throw new IOException("couldn't parse address");
-        }
-
-        String locality = coords.getLocality();
-
-        double lat = coords.getLatitude();
-        double lng = coords.getLongitude();
-
-        LatLng actualLocation = new LatLng(lat, lng);
-
-        //Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
-        //mMap.addMarker(new MarkerOptions().position(actualLocation).title("Marker "));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(actualLocation, 10));
-
-        return actualLocation;
-    }
-
-    //TODO: works fairly well – keep for now
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -148,37 +161,85 @@ public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng chicago = new LatLng(41.8781, -87.6298);
+        if (markerPoints.size() > 1) {
+            markerPoints.clear();
+            mMap.clear();
+        }
 
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Dan: experimental ----------------------------------
+        Polyline routePolyLine = mMap.addPolyline(new PolylineOptions().clickable(true));
+        LatLng latLng, prev, nex;
+        MarkerOptions options;
 
-        mMap.addMarker(new MarkerOptions().position(chicago).title("Marker in Chicago"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(chicago, 10));
+        //implement the user's last known position as start point
+        mMap.addMarker(new MarkerOptions().position(currentUserGeoPosition));
+        markerPoints.add(currentUserGeoPosition);
+        options = new MarkerOptions();
+        options.position(currentUserGeoPosition);
+        prev = currentUserGeoPosition;
 
+        for (int j=0; j<counter; j++) {
+            latLng = locationsToSeeCoordinates.get(j);
+            mMap.addMarker(new MarkerOptions().position(latLng));
+            // Adding new item to the ArrayList
+            markerPoints.add(latLng);
+            options = new MarkerOptions();
+            options.position(latLng);
+            nex = latLng;
+
+            if (markerPoints.size() == 1) {
+                options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+            } else if (markerPoints.size() >= 2) {
+                options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+                // Add new marker to the Google Map Android API V2
+                mMap.addMarker(options);
+
+                //LatLng origin = (LatLng) markerPoints.get(j-1);
+                //LatLng dest = (LatLng) markerPoints.get(j);
+
+                LatLng origin = prev;
+                LatLng dest = nex;
+
+                // Getting URL to the Google Directions API
+                String url = getDirectionsUrl(origin, dest);
+                Log.d("ROUTE_URL", url + "&key=" + APP_API_KEY);
+
+                DownloadTask downloadTask = new DownloadTask();
+
+                // Start downloading json data from Google Directions API
+                downloadTask.execute(url + "&key=" + APP_API_KEY);
+
+                prev = nex;
+                //nex = (LatLng) markerPoints.get(j);
+            }
+        }
+
+        //Log.d("MARKERS#", "NUMBER OF MARKERS ON MAP IS " + markerPoints.size());
+
+        mMap.setOnPolylineClickListener(this);
+        //Dan: experimental ---------------------------------- end
+
+        //mMap.addMarker(new MarkerOptions().position(chicago).title("Marker in Chicago"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(chicago, 10));
+        mMap.addMarker(new MarkerOptions().position(userLocalePos).title("Marker in " + userLocale));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocalePos,10));
+
+        /*
         //TODO: may not be necessary
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
 
-                if (markerPoints.size() > 1) {
-                    markerPoints.clear();
-                    mMap.clear();
-                }
-
                 // Adding new item to the ArrayList
                 markerPoints.add(latLng);
-
-                // Creating MarkerOptions
                 MarkerOptions options = new MarkerOptions();
-
-                // Setting the position of the marker
                 options.position(latLng);
 
                 if (markerPoints.size() == 1) {
                     options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-                } else if (markerPoints.size() == 2) {
+                } else if (markerPoints.size() >= 2) {
                     options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
                 }
 
@@ -192,14 +253,29 @@ public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCal
 
                     // Getting URL to the Google Directions API
                     String url = getDirectionsUrl(origin, dest);
+                    Log.d("ROUTE_URL", url + "&key=" + APP_API_KEY);
 
                     DownloadTask downloadTask = new DownloadTask();
 
                     // Start downloading json data from Google Directions API
-                    downloadTask.execute(url);
+                    downloadTask.execute(url + "&key=" + APP_API_KEY);
                 }
             }
         });
+        */
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+        // Flip from solid stroke to dotted stroke pattern.
+        if ((polyline.getPattern() == null) || (!polyline.getPattern().contains(DOT))) {
+            polyline.setPattern(PATTERN_POLYLINE_DOTTED);
+        } else {
+            // The default pattern is a solid stroke.
+            polyline.setPattern(null);
+        }
+
+        //Toast.makeText(this, "Route type " + polyline.getTag().toString(),Toast.LENGTH_SHORT).show();
     }
 
 
@@ -349,6 +425,32 @@ public class MapsRouteActivity extends FragmentActivity implements OnMapReadyCal
             urlConnection.disconnect();
         }
         return data;
+    }
+
+    //works fairly well – keep for now
+    private LatLng geoLocateString (String addressInput) throws IOException{
+        Geocoder gc = new Geocoder(MapsRouteActivity.this);
+        List<Address> listAddress = gc.getFromLocationName(addressInput, 1);
+        Address coords;
+        if (listAddress.size()>0) {coords = listAddress.get(0);}
+        else {
+            coords = null;
+            throw new IOException("couldn't parse address");
+        }
+
+        String locality = coords.getLocality();
+        Log.e("LOCALITY", locality);
+
+        double lat = coords.getLatitude();
+        double lng = coords.getLongitude();
+
+        LatLng actualLocation = new LatLng(lat, lng);
+
+        //Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+        //mMap.addMarker(new MarkerOptions().position(actualLocation).title("Marker "));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(actualLocation, 10));
+
+        return actualLocation;
     }
 
 }
--- app/src/main/java/longmoneyoffshore/dlrtime/utils/GlobalValues.java
+++ app/src/main/java/longmoneyoffshore/dlrtime/utils/GlobalValues.java
@@ -1,7 +1,11 @@
 package longmoneyoffshore.dlrtime.utils;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class GlobalValues {
     public static final String APP_API_KEY = "AIzaSyDCos2tFqSvd80yDcIZgl1_x9Zk1zWQ1RI";
-    public final String US_ANONYMIZER_PREFIX = "*67";
+    public static final String US_ANONYMIZER_PREFIX = "*67";
+
+    public static final LatLng ChicagoLocale = new LatLng(41.8781, -87.6298);
 
 }
